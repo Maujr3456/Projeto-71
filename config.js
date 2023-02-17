96f88e177fdf22f86c651dc14d5ba3370330bc90
Maujr3456 <93012770+Maujr3456@users.noreply.github.com>
2023-02-17 18:29:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,12 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+  apiKey: "AIzaSyD11SZPFAlbh0Tei6Y0mBatJ2pa-LK4tTA",
+  authDomain: "ciclista-eletronico-f1c66.firebaseapp.com",
+  projectId: "ciclista-eletronico-f1c66",
+  storageBucket: "ciclista-eletronico-f1c66.appspot.com",
+  messagingSenderId: "898878999840",
+  appId: "1:898878999840:web:3e97306dd9552e69839b45"
 };
 
 firebase.initializeApp(firebaseConfig);
